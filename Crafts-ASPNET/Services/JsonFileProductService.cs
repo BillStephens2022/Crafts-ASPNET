810b826c4908b1b8dcecebdfa75a26104528889a
BillStephens2022 <two4onebill@yahoo.com>
2024-03-15 15:07:24 -0400

    set up JsonFileProductService

---
+++ Crafts-ASPNET/Services/JsonFileProductService.cs
@@ -0,0 +1,33 @@
+﻿using Crafts_ASPNET.Models;
+using System.Text.Json;
+
+namespace Crafts_ASPNET.Services
+{
+    public class JsonFileProductService
+    {
+        // constructor
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
